b4fa7585eb42f6134315e2bff8b7099039e7f97c
chanales flores <cflores12@users.noreply.github.com>
2025-03-16 10:57:06 -0400

    Initial Commit

---
+++ RandomGenerator/RandomGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomGeneratorApp.swift
+//  RandomGenerator
+//
+//  Created by chanales flores on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomGeneratorTests/RandomGeneratorTests.swift
@@ -0,0 +1,16 @@
+//
+//  RandomGeneratorTests.swift
+//  RandomGeneratorTests
+//
+//  Created by chanales flores on 3/16/25.
+//
+
+import Testing
+
+struct RandomGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
